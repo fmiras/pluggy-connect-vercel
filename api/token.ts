ddf0b5b1c442802b2deb5efe02375746892d3ce4
Federico Miras <miras.federico@gmail.com>
2020-11-16 10:05:06 -0300

    fix: usage of promise

--- api/token.ts
+++ api/token.ts
@@ -1,14 +1,13 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 import { PluggyClient } from 'pluggy-sdk';
 
-const {PLUGGY_CLIENT_ID, PLUGGY_CLIENT_SECRET } = process.env;
+const { PLUGGY_CLIENT_ID, PLUGGY_CLIENT_SECRET } = process.env;
 
-export default async function (_: NowRequest, res: NowResponse) {
+export default function (_: NowRequest, res: NowResponse) {
   const pluggyClient = new PluggyClient({
     clientId: PLUGGY_CLIENT_ID,
     clientSecret: PLUGGY_CLIENT_SECRET,
   });
 
-  const { accessToken } = await pluggyClient.createConnectToken();
-  res.json({ accessToken });
+  pluggyClient.createConnectToken().then(data => res.json(data));
 }
