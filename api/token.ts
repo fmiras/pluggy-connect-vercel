ba6f4298dff70c8dec9ca817f0a70228498fa513
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2021-05-11 12:15:09 -0300

    feat: add connect token options on payload

--- api/token.ts
+++ api/token.ts
@@ -3,11 +3,13 @@ import { PluggyClient } from 'pluggy-sdk';
 
 const { PLUGGY_CLIENT_ID, PLUGGY_CLIENT_SECRET } = process.env;
 
-export default function (_: NowRequest, res: NowResponse) {
+export default function (req: NowRequest, res: NowResponse) {
   const pluggyClient = new PluggyClient({
     clientId: PLUGGY_CLIENT_ID,
     clientSecret: PLUGGY_CLIENT_SECRET,
   });
 
-  pluggyClient.createConnectToken().then(data => res.json(data));
+  const { itemId, options } = req.body || {}
+
+  pluggyClient.createConnectToken(itemId, options).then(data => res.json(data));
 }
